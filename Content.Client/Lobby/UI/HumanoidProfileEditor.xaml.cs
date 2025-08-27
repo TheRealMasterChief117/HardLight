27fb2e414cda7d82d85c73d758ef740e6aa3aa71
warcrazed11 <rjackcrozier@gmail.com>
2025-08-27 19:05:59 -0400

    yus
    * Synth Trait (cosmatic-drift-14/cosmatic-drift#149 and cosmatic-drift-14/cosmatic-drift#329) from cosmatic-drift-14/cosmatic-drift
    
    * Initial trait implementation
    
    * Fix the trait.
    
    * Readd fields.
    
    * fix: required comments for code changes upstream
    
    * feat: added excluded species field to traits.yml
    
    * fix: switching races now disables the unsupported trait
    
    * chore: added missing comment
    
    * fix: requested changes
    
    * fix: CR Round 2
    
    * chore: missed changes in foreign file

--- Content.Client/Lobby/UI/HumanoidProfileEditor.xaml.cs
+++ Content.Client/Lobby/UI/HumanoidProfileEditor.xaml.cs
@@ -220,6 +220,7 @@ namespace Content.Client.Lobby.UI
             {
                 SpeciesButton.SelectId(args.Id);
                 SetSpecies(_species[args.Id].ID);
+                RefreshTraits(); // DeltaV - Allows for hiding traits
                 UpdateHairPickers();
                 OnSkinColorOnValueChanged();
             };
@@ -538,6 +539,14 @@ namespace Content.Client.Lobby.UI
 
             foreach (var trait in traits)
             {
+                // Begin DeltaV Additions - Species trait exclusion
+                if (Profile?.Species is { } selectedSpecies && trait.ExcludedSpecies.Contains(selectedSpecies))
+                {
+                    Profile = Profile?.WithoutTraitPreference(trait.ID, _prototypeManager);
+                    continue;
+                }
+                // End DeltaV Additions
+
                 if (trait.Category == null)
                 {
                     defaultTraits.Add(trait.ID);

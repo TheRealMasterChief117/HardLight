6f764fe8048a052e0497d6404424724c1b58e188
TheRealMasterChief117 <200016362+TheRealMasterChief117@users.noreply.github.com>
2025-09-27 12:12:28 -0400

    ok/

--- Content.Shared/Floofstation/ModifyUndies/ModifyUndiesDoAfterEvent.cs
+++ Content.Shared/Floofstation/ModifyUndies/ModifyUndiesDoAfterEvent.cs
@@ -1,4 +1,5 @@
 using Content.Shared.DoAfter;
+using Content.Shared.Humanoid;
 using Content.Shared.Humanoid.Markings;
 using Robust.Shared.Serialization;
 
@@ -10,27 +11,38 @@ public sealed partial class ModifyUndiesDoAfterEvent : DoAfterEvent
     /// <summary>
     ///     The marking prototype that is being modified.
     /// </summary>
+    [DataField("markingPrototype", required: true)]
     public Marking Marking;
 
     /// <summary>
     ///     Localized string for the marking prototype.
     /// </summary>
+    [DataField("markingPrototypeName", required: true)]
     public string MarkingPrototypeName;
 
     /// <summary>
     ///     Whether or not the marking is visible at the moment.
     /// </summary>
+    [DataField("visible", required: true)]
     public bool IsVisible;
 
-    public ModifyUndiesDoAfterEvent(Marking marking, string markingPrototypeName, bool isVisible)
+    private ModifyUndiesDoAfterEvent()
+    {
+        Marking = default!;
+        MarkingPrototypeName = string.Empty;
+        IsVisible = false;
+    }
+
+    public ModifyUndiesDoAfterEvent(
+        Marking marking,
+        string markingPrototypeName,
+        bool isVisible
+        )
     {
         Marking = marking;
         MarkingPrototypeName = markingPrototypeName;
         IsVisible = isVisible;
     }
 
-    public override DoAfterEvent Clone()
-    {
-        return this;
-    }
+    public override DoAfterEvent Clone() => this;
 }

8fcf35e36428d2dfef0a44503690aa67febda1a3
TheRealMasterChief117 <200016362+TheRealMasterChief117@users.noreply.github.com>
2025-09-27 13:52:17 -0400

    Update ModifyUndiesDoAfterEvent.cs

--- Content.Shared/Floofstation/ModifyUndies/ModifyUndiesDoAfterEvent.cs
+++ Content.Shared/Floofstation/ModifyUndies/ModifyUndiesDoAfterEvent.cs
@@ -1,48 +1,25 @@
 using Content.Shared.DoAfter;
-using Content.Shared.Humanoid;
 using Content.Shared.Humanoid.Markings;
 using Robust.Shared.Serialization;
 
-namespace Content.Shared.Floofstation;
+namespace Content.Shared.Floofstation.ModifyUndies;
 
 [Serializable, NetSerializable]
-public sealed partial class ModifyUndiesDoAfterEvent : DoAfterEvent
+public sealed partial class ModifyUndiesDoAfterEvent : SimpleDoAfterEvent
 {
-    /// <summary>
-    ///     The marking prototype that is being modified.
-    /// </summary>
-    [DataField("markingPrototype", required: true)]
+    [DataField(required: true)]
     public Marking Marking;
 
-    /// <summary>
-    ///     Localized string for the marking prototype.
-    /// </summary>
-    [DataField("markingPrototypeName", required: true)]
+    [DataField(required: true)]
     public string MarkingPrototypeName;
 
-    /// <summary>
-    ///     Whether or not the marking is visible at the moment.
-    /// </summary>
-    [DataField("visible", required: true)]
+    [DataField(required: true)]
     public bool IsVisible;
 
-    private ModifyUndiesDoAfterEvent()
-    {
-        Marking = default!;
-        MarkingPrototypeName = string.Empty;
-        IsVisible = false;
-    }
-
-    public ModifyUndiesDoAfterEvent(
-        Marking marking,
-        string markingPrototypeName,
-        bool isVisible
-        )
+    public ModifyUndiesDoAfterEvent(Marking marking, string markingPrototypeName, bool isVisible)
     {
         Marking = marking;
         MarkingPrototypeName = markingPrototypeName;
         IsVisible = isVisible;
     }
-
-    public override DoAfterEvent Clone() => this;
 }
